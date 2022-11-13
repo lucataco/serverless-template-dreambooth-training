d80ed3aa5f349b620bea9f1abe2756acdfd243a4
Luis <lucataco@umd.edu>
2022-11-12 20:58:22 -0500

    fix prompt for template

--- train.sh
+++ train.sh
@@ -23,6 +23,6 @@ accelerate launch train_dreambooth.py \
   --sample_batch_size=1 \
   --max_train_steps=$NUM_STEPS \
   --save_interval=$NUM_STEPS \
-  --save_sample_prompt="photo of ppa person" \
+  --save_sample_prompt="photo of sks person" \
   --concepts_list="concepts_list.json" \
   --pad_tokens
