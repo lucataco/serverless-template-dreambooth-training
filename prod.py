0c7aa87ec799aae510652d0f5646bccfe7308306
Luis <lucataco@umd.edu>
2022-11-12 20:57:52 -0500

    update description

--- prod.py
+++ prod.py
@@ -7,13 +7,13 @@ from PIL import Image
 #File_id of file in google drive
 file_id = sys.argv[1:][0]
 
-#Request an image in gdrive to be downloaded and run 100 prompts on
+#FileId needed to download training images from folder UploadedImgs/
 model_inputs = {"file_id": file_id }
 
 api_key = ""
 model_key = ""
 
-# Prod - call the pic model
+# Prod - call the training model
 res = banana.run(api_key, model_key, model_inputs)
 
 # Get back file_id of model.ckpt in google drive
